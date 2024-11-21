755f6e549e94a0c6c42a541a23c5ac414112cebb
Monique Menezes <122538719+Niquemenezes@users.noreply.github.com>
2024-11-21 18:18:17 +0000

    generador de excusa

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let Quien = ["Mi perro", "Mi madre", "El vecino", "Mi hijo", "Mi professor"];
+  let Accion = ["comio", "perdio", "robo", "rompio", "arreglo"];
+  let Que = ["mi trabajo", "mi telefone", "mis llaves", "mi coche"];
+  let Quando = [
+    "ayer",
+    "despues del trabajo",
+    "hoy por la mañana",
+    "en la cena"
+  ];
+
+  function generarExcusa() {
+    let personajes = Quien[Math.floor(Math.random() * Quien.length)];
+    let verbos = Accion[Math.floor(Math.random() * Accion.length)];
+    let objeto = Que[Math.floor(Math.random() * Que.length)];
+    let tiempo = Quando[Math.floor(Math.random() * Quando.length)];
+
+    return `${personajes} ${verbos} ${objeto} ${tiempo}.`;
+  }
+
+  document.getElementById("excusa").innerText = generarExcusa();
 };
+document.getElementById("botonExcusa").addEventListener("click", function() {
+  location.reload();
+});
